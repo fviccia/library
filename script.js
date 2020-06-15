71c8b9e9c561191c0fe917684a11690d0acf72ef
Franco Viccia <francoviccia@gmail.com>
2020-06-15 17:42:28 -0300

    Added sample books, styling and Add New Book Fn

--- script.js
+++ script.js
@@ -1,27 +1,63 @@
-let myLibrary = [];
-
-let id = 0;
-
+let myLibrary = [
+  {id: 0, title: 'Title: 1984', author: 'Author: George Orwell', pages: 'Number of Pages: 326', readed: 'Readed: Yes'},
+  {id: 0, title: 'Title: To Kill a Mockingbird', author: 'Author: Harper Lee', pages: 'Number of Pages: 281', readed: 'Readed: No'} ];
+  let library = document.getElementById('library');
+  let addBook = document.getElementById('add-Book');
+  let title = document.getElementById('title');
+  let author = document.getElementById('author');
+  let pages = document.getElementById('pages');
+  let readed = document.getElementById('readed');
+  
+  let id = 0;
+  
+  render();
 
 // The constructor.
 function Book(id, title, author, pages, readed) {
   this.id = id;
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.readed = readed;
+  this.title = 'Title: ' + title;
+  this.author = 'Author: ' + author;
+  this.pages = 'Number of Pages: ' + pages;
+  this.readed = 'Readed: ' + readed;
 }
 
-function addBookToLibrary(title, author, pages, readed) {
+function addBookToLibrary() {
   id ++;
-  let book = new Book(id, title, author, pages, readed);
+  let book = new Book(id, title.value, author.value, pages.value, readed.value);
   myLibrary.push(book);
+  library.innerHTML = '';
+  render();
 }
-
-// Some sample books.
-addBookToLibrary('1984', 'George Orwell', '326', true);
-addBookToLibrary('To Kill a Mockingbird', 'Harper Lee', '281', false);
 
 function render() {
-  
+  for (let i = 0 ; i < myLibrary.length ; i++) {
+    // Create book div.
+    let div = document.createElement('div');
+    div.className = 'book';
+    // Create an array of values of the object.  
+    let properties = Object.values(myLibrary[i]);
+    // Loop trough properties of each book and append them.
+    for (let i = 1 ; i < properties.length ; i++){
+      let property = document.createElement('div');
+      property.className = 'properties';
+      property.innerHTML = properties[i];
+      div.appendChild(property);
+    }
+    
+    let deleteBook = document.createElement('button');
+    deleteBook.innerHTML = 'Delete';
+    deleteBook.className = 'delete-button';
+    div.appendChild(deleteBook);
+    library.appendChild(div);
+  }
 }
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+
